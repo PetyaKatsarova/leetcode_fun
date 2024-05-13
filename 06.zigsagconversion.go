6eff623d830a0337548e8001809581340a2da542
PetyaKatsarova <pskpetya@gmail.com>
2024-05-13 15:47:31 +0200

    added 06.zigzag solution

--- 06.zigsagconversion.go
+++ 06.zigsagconversion.go
@@ -1,6 +1,10 @@
 package main
+
+import "fmt"
+
 /* --- TAG: MEDIUM ---
-The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this
+pattern in a fixed font for better legibility)
 
 P   A   H   N
 A P L S I I G
@@ -9,7 +13,7 @@ And then read line by line: "PAHNAPLSIIGYIR"
 
 Write the code that will take a string and make this conversion given a number of rows:
 string convert(string s, int numRows);
- 
+
 Example 1:
 Input: s = "PAYPALISHIRING", numRows = 3
 Output: "PAHNAPLSIIGYIR"
@@ -26,9 +30,43 @@ P     I
 Example 3:
 Input: s = "A", numRows = 1
 Output: "A"
- 
+
 Constraints:
 1 <= s.length <= 1000
 s consists of English letters (lower-case and upper-case), ',' and '.'.
 1 <= numRows <= 1000
 */
+
+func main() {
+	convert("PAYPALISHIRING", 3)
+	convert("PAYPALISHIRING", 4)
+}
+
+func convert(s string, numRows int) string {
+	if numRows <= 1 || numRows >= len(s) {
+		return s
+	}
+    myslice		:= make([]string, numRows)
+	currRow		:= 0
+	goingDown	:= false
+
+	for _, val := range s {
+		myslice[currRow] += string(val)
+
+		if currRow == 0 || currRow == numRows - 1 {
+			goingDown = !goingDown
+		}
+		if goingDown {
+			currRow++
+		} else {
+			currRow--
+		}
+	}
+	
+	mystr := ""
+	for _, val := range myslice {
+		mystr += string(val)
+	}
+	fmt.Println(mystr)
+	return mystr
+}
