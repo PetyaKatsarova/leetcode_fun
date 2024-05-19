a18e24fa8a809d4d7fb89d184f2c670983c53d04
PetyaKatsarova <pskpetya@gmail.com>
2024-05-19 08:14:54 +0200

    added solution for 09

--- 09.palindrome_num.go
+++ 09.palindrome_num.go
@@ -1,5 +1,3 @@
-package main
-
 /* -- TAG: EASY ------------
 Given an integer x, return true if x is a palindrome, and false otherwise.
 
@@ -17,11 +15,36 @@ Example 3:
 Input: x = 10
 Output: false
 Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
- 
+
 Constraints: -231 <= x <= 231 - 1
 Follow up: Could you solve it without converting the integer to a string?
 */
 
-func isMatch(s string, p string) bool {
-    
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("expected: true, output: ", isPalindrome(121))
+	fmt.Println("expected: false, output: ", isPalindrome(-121))
+	fmt.Println("expected: false, output: ", isPalindrome(10))
+	fmt.Println("expected: true, output: ", isPalindrome(1111))
+}
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	strNum := strconv.Itoa(x)
+
+	for i := 0; i < len(strNum)/2; i++ {
+		if strNum[i] != strNum[len(strNum)-i-1] {
+			return false
+		}
+	}
+	return true
 }
