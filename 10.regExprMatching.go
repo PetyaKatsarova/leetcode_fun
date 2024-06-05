a037f8d16eec0dce86fd5aa597c9c7029ab7a515
PetyaKatsarova <pskpetya@gmail.com>
2024-06-05 23:01:16 +0200

    added faster solution for 11

--- 10.regExprMatching.go
+++ 10.regExprMatching.go
@@ -30,73 +30,9 @@ It is guaranteed for each appearance of the character '*', there will be a previ
 
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
+
 
 func main() {
-	// fmt.Println("expected: false(for p 'a'), output: ", isMatch("aa", "a"))
-	// fmt.Println("expected: false(for p 'aaa'), output: ", isMatch("aa", "aaa"))
-	fmt.Println("expected: true(for p 'a*'), output: ", isMatch("aa", "a*"))
-	// fmt.Println("expected: true(for p '.*'), output: ", isMatch("abc", "."))
-	// fmt.Println("expected: true(for p 'a.*'), output: ", isMatch("ab", "a."))
-	// fmt.Println("expected: false(for p 'a.*'), output: ", isMatch("abc", "a."))
-	// fmt.Println("expected: true(for p 'c4a2b'), output: ", isMatch("c4a2b", "c.a.b"))
-	// fmt.Println("expected: true(for p 'c4a42b'), output: ", isMatch("c4a42b", "c.a..b"))
-	// fmt.Println("expected: false(for p 'c4a4b'), output: ", isMatch("c4a4b", "c.a..b"))
-	// fmt.Println("expected: true(for p 'c4a42b...'), output: ", isMatch("c4a42bbla", "c.a..b..."))
+	fmt.Println("hello 10, this si really hard...")
 }
-
-func isMatch(s string, p string) bool {
-	lenS, lenP := len(s), len(p)
-	twoD := make([][]bool, lenS+1) // default vals = false; if s=="abscd": [[false, false, false, false],[],[],[],[]]
-
-	for i := range twoD {
-		twoD[i] = make([]bool, lenP+1) /* if p=="a*d": [
-			[false, false, false, false],
-			[false, false, false, false],
-			[false, false, false, false],
-			[false, false, false, false],
-			[false, false, false, false],
-		]
-
-		*/
-	}
-	twoD[0][0] = true
-	for j := 1; j <= lenP; j++ {
-		if p[j-1] == '*' {
-			twoD[0][j] = twoD[0][j-2]
-		}
-	}
-}
-
-// func isMatch(s string, p string) bool {
-// 	m, n := len(s), len(p)
-// 	dp := make([][]bool, m+1)
-// 	for i := range dp {
-// 		dp[i] = make([]bool, n+1)
-// 	}
-// 	dp[0][0] = true
-
-// 	for j := 1; j <= n; j++ {
-// 		if p[j-1] == '*' {
-// 			dp[0][j] = dp[0][j-2]
-// 		}
-// 	}
-
-// 	for i := 1; i <= m; i++ {
-// 		for j := 1; j <= n; j++ {
-// 			if p[j-1] == '.' || p[j-1] == s[i-1] {
-// 				dp[i][j] = dp[i-1][j-1]
-// 			} else if p[j-1] == '*' {
-// 				dp[i][j] = dp[i][j-2]
-// 				if p[j-2] == '.' || p[j-2] == s[i-1] {
-// 					dp[i][j] = dp[i][j] || dp[i-1][j]
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return dp[m][n]
-// }
-
--- 11.container_with_most_water.go
+++ 11.container_with_most_water.go
@@ -6,31 +6,53 @@ You are given an integer array height of length n. There are n vertical lines dr
 Find two lines that together with the x-axis form a container, such that the container contains the most water.
 Return the maximum amount of water a container can store.
 Notice that you may not slant the container.
-Input: height = [1,8,6,2,5,4,8,3,7]
-Output: 49
-Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
-Example 2:
-Input: height = [1,1]
-Output: 1
+Example 1: Input: height = [1,8,6,2,5,4,8,3,7] Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can 
+//contain is 49.
+Example 2: Input: height = [1,1] Output: 1
 Constraints:
 n == height.length
 2 <= n <= 105
 0 <= height[i] <= 10000
 */
 
-// import "fmt"
-// func main() {
-// 	fmt.Println("hallo world")
-// 	maxArea([]int{1,8,6,2,5,4,8,3,7}) // 49
-// 	maxArea([]int{1,1}) //1
-// }
+import "fmt"
+func main() {
+	fmt.Println("hallo world")
+	fmt.Println("expected: 49, outcome: ", maxArea([]int{1,8,6,2,5,4,8,3,7})) // 49
+	fmt.Println("expected: 1, outcome: ", maxArea([]int{1,1})) //1
+}
 
+func maxArea(height []int) int {
+	left, right := 0, len(height)-1
+	maxArea 	:= 0
+
+	for left < right {
+		minHeight := height[left]
+		if height[left] > height[right] {
+			minHeight = height[right]
+		}
+		area := minHeight * (right - left)
+		if area > maxArea { maxArea = area }
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+	return maxArea
+}
+
+// too slow: O(n^2) time complexity: nested loops
 // func maxArea(height []int) int {
-// 	result := 0
-// 	for x, y := range height {
-// 		fmt.Println("x = ", x, "y = ", y)
-// 	}
+//     maxArea := 0
+// 	for i:=0; i<len(height); i++ {
+// 		for j:=0; j<len(height); j++ {
+// 			minHeight := height[i] 
+// 			if minHeight > height[j] { minHeight = height[j] }
 
-// 	fmt.Println("max area: ", result)
-// 	return result
+// 			if maxArea < minHeight * (j - i) { maxArea = minHeight * (j - i) }
+// 		}
+// 	}
+// 	return maxArea
 // }
